cf010ff5172a177661ad89199d97f4dacdd40dbf
Mrtup <pinedapablo716@gmail.com>
2017-12-21 16:06:04 -0500

    jokerbusta commit

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,10 +1,26 @@
 var d = document.getElementById ("dibujo");
 var lienzo = d.getContext ("2d");
+var lineas =30;
+var l = 0;
+var yi, xf, xi, yf;
+var colo = "red";
 
-DibujarLinea("red", 10, 300, 220,10);
-DibujarLinea("blue", 15, 300, 220,10);
-DibujarLinea("blue", 20, 300, 220,10);
-
+while (l < lineas)
+    {
+        yi =10 * l;
+        xf =10 * (l + 1);
+        xi =10* l;
+        yf =10* (l + 1);
+        
+        
+        
+     DibujarLinea(colo, 0, yi, xf, 300);  
+     DibujarLinea(colo, xi, 0, 300, yf);
+        console.log ("linea" + 1)
+        l = l + 1;
+    }
+DibujarLinea (colo,1,1,1,299);
+DibujarLinea (colo,1,299,299,299);
 function DibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
     console.log (lienzo);
