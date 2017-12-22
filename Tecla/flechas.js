80ccadbf54459fff7fa3c448126c9287cf9acc81
Mrtup <pinedapablo716@gmail.com>
2017-12-22 09:17:58 -0500

    funcion con teclado asignado

---
+++ Tecla/flechas.js
@@ -0,0 +1,29 @@
+var teclas = {
+UP: 38,
+LEFT: 37,
+RIGHT: 39,
+DOWN: 40 
+};
+console.log (teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
+{
+    if (evento.keyCode == teclas.UP)
+        {
+            console.log ("tecla arriba");
+        }
+    if (evento.keyCode == teclas.DOWN)
+        {
+            console.log ("tecla abajo");
+        }
+    if (evento.keyCode == teclas.LEFT)
+        {
+            console.log ("tecla izquierda");
+        }
+    if (evento.keyCode == teclas.RIGHT)
+    {
+        console.log ("tecla derecha");
+    }
+}
