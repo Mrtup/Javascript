d48dec45689c31d05a92b7a1aa593793ca0b4cda
Mrtup <pinedapablo716@gmail.com>
2017-12-22 11:53:41 -0500

    Trabajo pendiente

--- Tecla/flechas.js
+++ Tecla/flechas.js
@@ -7,6 +7,23 @@ DOWN: 40
 console.log (teclas);
 
 document.addEventListener("keyup", dibujarTeclado);
+var cuadrito = document.getElementById ("area_de_dibujo");
+var papel = cuadrito.getContext ("2d");
+var x =150;
+var y =150;
+
+DibujarLinea("red", 149,149,151,151, papel);
+
+function DibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo (xinicial,yinicial);
+    lienzo.lineTo (xfinal,yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
 
 function dibujarTeclado(evento)
 // FUNCIONES CON swich
